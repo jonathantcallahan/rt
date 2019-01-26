aa901896a12fb831dae9452570e2e5b226efa5a7
Jonathan <Jonathan@iMac-3.local>
2019-01-26 00:13:23 -0500

    styling

--- client/src/Tasklist.js
+++ client/src/Tasklist.js
@@ -19,26 +19,39 @@ class Tasklist extends Component {
     render(){
         return (
             <div>
-                <input name='task' value={this.state.task} onChange={this.update}></input>
-                <div onClick={() => this.props.tasks.add(this.state.task)}>create task</div>
+                <div className='task-input-container'>
+                    <input 
+                        className='task-create-input' 
+                        name='task' 
+                        value={this.state.task} 
+                        onChange={this.update}
+                        placeholder='task name'></input>
+                    <div 
+                        className='task-create-submit' 
+                        onClick={() => {
+                            this.props.tasks.add(this.state.task);
+                            this.setState({task:''})}}>
+                        create task</div>
+                </div>
                 <div className='task-container'>
                     <span className='task-header'>tasks</span>
                     {this.props.state.taskList && this.props.state.taskList.map((e,i) => {
                         console.log(e)
                         return (
-                            <div>
-                                <span 
-                                    key={`delete_${e[0]}`} 
-                                    onClick={() => this.props.tasks.delete(e[0])}>
-                                    delete</span>
-                                <span 
-                                    key={`task_${e[0]}`}>
-                                    {e[0]}</span>
-                                <input 
+                            <div className={`task ${e[1] && 'gray'}`}>
+                                <input
                                     key={`status_${e[0]}`}
                                     type='checkbox'
                                     defaultChecked={e[1]}
                                     onClick={() => this.props.tasks.complete(e[0])}></input>
+                                <span
+                                    className={`task-name ${e[1] && 'font-weight-normal'}`}
+                                    key={`task_${e[0]}`}>
+                                    {e[0]}</span>
+                                <span 
+                                    key={`delete_${e[0]}`} 
+                                    onClick={() => this.props.tasks.delete(e[0])}>
+                                    delete</span>
                             </div>
                         )
                     })}
